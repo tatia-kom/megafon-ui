5ba76220113e9b6b708b8ff92da1c44bfe4fc537
Azamat Dzebisov (via Travis CI) <azamat.dzebisov@gmail.com>
2020-08-26 19:47:45 +0000

    Deploy MegafonWebLab/megafon-ui to github.com/MegafonWebLab/megafon-ui.git:gh-pages

---
+++ component---src-pages-404-js-55cf56143962c5080709.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-55cf56143962c5080709.js.map
